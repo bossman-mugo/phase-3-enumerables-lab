e42281c0f926bada55ad23616ac597df70897efb
Eric Mugo <mugoericpg20@gmail.com>
2023-05-18 14:54:49 +0300

    Added food by cuisine

--- enumerables.rb
+++ enumerables.rb
@@ -36,7 +36,8 @@ end
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-  # your code here
+  spicy_foods.find { |food| food[:cuisine] == cuisine }
+ 
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
